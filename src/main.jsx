e72b33fde577c2a10db259ecdf15808f20dc88b5
maershaa <valeria.work2022@gmail.com>
2024-03-19 21:52:52 +0200

    init commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="react-vite-builder">
+    <BrowserRouter basename="car-rental">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
