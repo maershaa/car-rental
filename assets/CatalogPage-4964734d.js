7bd94a104a6fe1f0aaa8036257705e86c4a05c60
maershaa <129538294+maershaa@users.noreply.github.com>
2024-03-19 20:54:10 +0000

    Deploying to gh-pages from @ maershaa/car-rental@cd24fade23f13048f517e16aa084fd358085df08 🚀

---
+++ assets/CatalogPage-4964734d.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-3d50cdf5.js";const e=()=>a.jsx("div",{children:"CatalogPage"});export{e as default};
---
+++ assets/cars.selectors-7fbfb125.js
@@ -0,0 +1,1 @@
+const s=r=>(console.log("selectCars",r.carsStore.cars),r.carsStore.cars),e=r=>r.carsStore.totalFavorites,t=r=>r.carsStore.error,o=r=>r.carsStore.isLoading;export{o as a,t as b,e as c,s};
